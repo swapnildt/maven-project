b1aa14b2a859065fb20a3051847e6af4298415bc
Ubuntu <ubuntu@ip-172-31-26-185.ap-south-1.compute.internal>
2018-08-04 14:59:53 +0000

    javadoc changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,10 @@ public class Greeter {
 
   }
 
+/**
+*@param someone the name of a person
+*@return greeting s
+*/
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
