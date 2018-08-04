b2193dd81752248226ee6383b07838ba10a76b92
Ubuntu <ubuntu@ip-172-31-26-185.ap-south-1.compute.internal>
2018-08-04 15:02:58 +0000

    fkf

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
 *@param someone the name of a person
 *@return greeting s
 */
-  public String greet(final String someone) {
+  public String final greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
